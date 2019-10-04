74489b36d2b8940d48ee6c12686a9280a3825c4a
Joel Watson <joelwat@gmail.com>
2019-10-03 17:05:56 -0700

    Replaces dev password with placeholder

--- migrations/init/2.create_user.sql
+++ migrations/init/2.create_user.sql
@@ -1,3 +1,3 @@
-CREATE USER 'bufferOverflow-user'@'%' IDENTIFIED BY 'x4bfaKkEM$IvoLIHFim@vejlUm3';
+CREATE USER 'bufferOverflow-user'@'%' IDENTIFIED BY SECRET_PASSWORD; -- Replace with a real password.
 GRANT SELECT, INSERT, UPDATE, DELETE ON buffer_overflow.* TO 'bufferOverflow-user'@'%';
 FLUSH PRIVILEGES;
