702592721547e944318e21c032817c6180b59106
Joel Watson <joelwat@gmail.com>
2019-10-09 14:15:46 -0700

    Create user with old mysql_native_password.
    The npm package mysql still doesn't support the new caching_sha2_password

--- migrations/init/2.create_user.sql
+++ migrations/init/2.create_user.sql
@@ -1,3 +1,3 @@
-CREATE USER 'buffer-overflow-user'@'%' IDENTIFIED BY SECRET_PASSWORD; -- Replace with a real password.
+CREATE USER 'buffer-overflow-user'@'%' IDENTIFIED WITH mysql_native_password BY SECRET_PASSWORD; -- Replace with a real password.
 GRANT SELECT, INSERT, UPDATE, DELETE ON buffer_overflow.* TO 'buffer-overflow-user'@'%';
 FLUSH PRIVILEGES;
