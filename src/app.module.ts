9d78953bbfc9fa16539166dae12171c468f1a276
Joel Watson <joelwat@gmail.com>
2019-10-09 16:41:24 -0700

    Adds users Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
@@ -11,6 +12,7 @@ import { UsersController } from './users/users.controller';
   ],
   providers: [
     AppService,
+    UsersService,
   ],
 })
 export class AppModule {}
