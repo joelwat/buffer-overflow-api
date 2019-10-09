4ddb7a9676446258a4a47dca97caacc741d00975
Joel Watson <joelwat@gmail.com>
2019-10-09 15:09:18 -0700

    Adds database connection and gracefulShutdown

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { MySQLDataContext, ConnectionOptions, DataContext } from 'formn';
+import { INestApplication } from '@nestjs/common';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const connOpts: ConnectionOptions = require('../connections.json');
+  const dataContext: DataContext    = new MySQLDataContext();
+
+  let app: INestApplication;
+
+  dataContext.connect(connOpts)
+    .then(async () => {
+      console.log('Database connected!');
+
+      app = await NestFactory.create(AppModule);
+      await app.listen(3000);
+    })
+    .catch((err) => {
+      console.error('Failed to connect to database.');
+      console.error(err);
+
+      process.exit(1);
+    });
+
+  function gracefulShutdown() {
+    console.log('Shutting down!');
+
+    app.close();
+    console.log('Http closed.');
+
+    dataContext.end();
+    console.log('Database disconnected.');
+
+    process.exit(0);
+  }
+
+  // Handle <Ctrl> + c
+  process.on('SIGTERM', gracefulShutdown);
+
 }
+
 bootstrap();
