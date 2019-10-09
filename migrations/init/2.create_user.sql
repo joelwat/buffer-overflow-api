c128e47c7f71bc0769865fbb41952b6abdcbc166
Joel Watson <joelwat@gmail.com>
2019-10-09 14:14:03 -0700

    Changes db username to kebab-case

--- migrations/init/2.create_user.sql
+++ migrations/init/2.create_user.sql
@@ -1,3 +1,3 @@
-CREATE USER 'bufferOverflow-user'@'%' IDENTIFIED BY SECRET_PASSWORD; -- Replace with a real password.
-GRANT SELECT, INSERT, UPDATE, DELETE ON buffer_overflow.* TO 'bufferOverflow-user'@'%';
+CREATE USER 'buffer-overflow-user'@'%' IDENTIFIED BY SECRET_PASSWORD; -- Replace with a real password.
+GRANT SELECT, INSERT, UPDATE, DELETE ON buffer_overflow.* TO 'buffer-overflow-user'@'%';
 FLUSH PRIVILEGES;
