135d116c4e88439565f022b0d52bd71bbc7f20a5
Joel Watson <joelwat@gmail.com>
2019-10-17 13:01:58 -0700

    Adds getAll function for users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import { DataContext } from 'formn';
+import { DataContextManager } from 'formn-nestjs-utils';
+
+import { User } from '../entities/user.entity';
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly dc: DataContext;
+
+  constructor(private readonly dcManager: DataContextManager) {
+    this.dc = dcManager.dataContext;
+  }
+
+  getAll() {
+    return this.dc
+      .from(User, 'u')
+      .select()
+      .execute();
+  }
+}
