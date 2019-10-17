00876208085484ba95f7e782cd274e7ea660aee7
Joel Watson <joelwat@gmail.com>
2019-10-17 14:15:40 -0700

    Switches to use DataContextManger

--- src/main.ts
+++ src/main.ts
@@ -3,27 +3,17 @@ import { INestApplication } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
-import { MySQLDataContext, ConnectionOptions, DataContext } from 'formn';
+import { BsyErrorHandlerFilter } from 'formn-nestjs-utils';
 
 async function bootstrap() {
-  const connOpts: ConnectionOptions = require('../connections.json');
-  const dataContext: DataContext    = new MySQLDataContext();
+  const app: INestApplication = await NestFactory.create(AppModule);
 
-  let app: INestApplication;
+  app.useGlobalFilters(new BsyErrorHandlerFilter());
 
-  dataContext.connect(connOpts)
-    .then(async () => {
-      console.log('Database connected!');
+  await app.listen(3000);
 
-      app = await NestFactory.create(AppModule);
-      await app.listen(3000);
-    })
-    .catch((err) => {
-      console.error('Failed to connect to database.');
-      console.error(err);
-
-      process.exit(1);
-    });
+  // Handle <Ctrl> + c
+  process.on('SIGTERM', gracefulShutdown);
 
   function gracefulShutdown() {
     console.log('Shutting down!');
@@ -31,15 +21,8 @@ async function bootstrap() {
     app.close();
     console.log('Http closed.');
 
-    dataContext.end();
-    console.log('Database disconnected.');
-
     process.exit(0);
   }
-
-  // Handle <Ctrl> + c
-  process.on('SIGTERM', gracefulShutdown);
-
 }
 
 bootstrap();
