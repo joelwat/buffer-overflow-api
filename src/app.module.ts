d3cfa4d08e6c040ab8700e0308b0e5a07844efcc
Joel Watson <joelwat@gmail.com>
2019-10-17 13:07:07 -0700

    Imports and configures Formn

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+
+import { ConnectionsFileReader } from 'formn';
+
+import { FormnModule } from 'formn-nestjs-utils';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
+  imports: [
+    FormnModule.forRoot(
+      new ConnectionsFileReader()
+        .readConnectionOptions('./connections.json')[0],
+    ),
+  ],
   controllers: [
     AppController,
     UsersController,
