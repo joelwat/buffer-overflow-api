b4bd879a8c059c3f307a5944dfd6f2bd8fc69bfa
Joel Watson <joelwat@gmail.com>
2019-10-17 13:02:21 -0700

    Adds route to return all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { UsersService } from './users.service';
+
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersSvc: UsersService) {
+  }
+
+  @Get()
+  getAll() {
+    return this.usersSvc.getAll();
+  }
+}
