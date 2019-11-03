a12efb35ccb193bac7875e8f8bed3dad2f6b4ddd
Joel Watson <joelwat@gmail.com>
2019-11-02 18:43:52 -0700

    Readds Param import lost in merge

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { User } from '../entities/user.entity';
