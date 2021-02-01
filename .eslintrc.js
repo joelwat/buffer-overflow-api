fab5edd4682d66f9b5da1673622421c433ef729b
Joel Watson <joelwat@gmail.com>
2021-01-31 17:28:58 -0800

    Updates ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,17 +6,47 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'airbnb-typescript/base',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      }
+    ],
+    'no-param-reassign': [
+      'error',
+      {
+        props: false,
+      },
+    ],
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js']
+      }
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts']
+    }
+  }
 };
